4d5f589322c6e0cc627ff06640de48dbadcc5941
Octogonapus <firey45@gmail.com>
2019-11-11 17:02:13 -0500

    Added numberOfIncrements

--- myWalkingScript.groovy
+++ myWalkingScript.groovy
@@ -3,7 +3,7 @@ if (base == null) {
 	throw new IllegalStateException("MediumKat device was null.");
 }
 
-def T_deltaBody = new TransformNR(0, 0, 0, new RotationNR(0, 20, 0)).inverse()
+def T_deltaBody = new TransformNR(0, 0, 10, new RotationNR(0, 0, 0)).inverse()
 
 void homeLegs(MobileBase base) {
 	base.getLegs().each {
@@ -11,24 +11,36 @@ void homeLegs(MobileBase base) {
 	}
 }
 
-void moveBase(MobileBase base, TransformNR baseDelta) {
-	base.getLegs().each { leg ->
+void moveBase(MobileBase base, TransformNR baseDelta, int numberOfIncrements) {
+	def tipsInWorldSpace = base.getLegs().collect { leg ->
 		def T_tipGlobal = leg.getCurrentPoseTarget()
 		def T_fiducialLimb = leg.getRobotToFiducialTransform()
 		def T_globalFiducial = base.getFiducialToGlobalTransform()
 	
 		def tipInLimbSpace = leg.inverseOffset(T_tipGlobal)
 		def newBodyPose = T_globalFiducial.times(baseDelta)
-		def newTipInWorldSpace = newBodyPose.times(T_fiducialLimb).times(tipInLimbSpace)
+		[leg, newBodyPose.times(T_fiducialLimb).times(tipInLimbSpace)]
+	}
+
+	for (int i = 0; i <= numberOfIncrements; i++) {
+		double scale = i / numberOfIncrements
 		
-		if (leg.checkTaskSpaceTransform(newTipInWorldSpace)) {
-			leg.setDesiredTaskSpaceTransform(newTipInWorldSpace, 0)
+		tipsInWorldSpace.each { legAndTip ->
+			def leg = legAndTip[0]
+			def tipTargetInWorldSpace = legAndTip[1].scale(scale)
+			if (leg.checkTaskSpaceTransform(tipTargetInWorldSpace)) {
+				leg.setDesiredTaskSpaceTransform(tipTargetInWorldSpace, 0)
+			}
 		}
+		
+		Thread.sleep(10)
 	}
 }
 
-homeLegs(base)
-moveBase(base, T_deltaBody)
+//homeLegs(base)
+moveBase(base, T_deltaBody, 1)
+Thread.sleep(500)
+moveBase(base, T_deltaBody.inverse(), 1)
 
 /*DHParameterKinematics leg = base.getLegs()[0]
 leg.setDesiredTaskSpaceTransform(leg.calcHome(), 0)
