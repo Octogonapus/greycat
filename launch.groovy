192c79d2b5a69ac5fdb8d55a11d723a9d10c9128
Ryan Benasutti <firey45@gmail.com>
2019-12-08 20:40:56 -0500

    Update CoM and mass

--- launch.groovy
+++ launch.groovy
@@ -1,4 +1,4 @@
 def cat =ScriptingEngine.gitScriptRun(	"https://github.com/NotOctogonapus/SmallKat_V2.git", 
 								"loadRobot.groovy", 
 ["https://github.com/NotOctogonapus/greycat.git",
-		"MediumKat_black.xml","GameController_22","hidDevice", false]);
+		"MediumKat_black.xml","GameController_22","hidDevice", true]);
